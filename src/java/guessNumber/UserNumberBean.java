e7a3c1e7d776f84efb5cac79323b9fcb0da0a2b6
leticiane <leticianesalles@hotmail.com>
2014-03-24 20:58:58 -0300

    primeiro commit

---
+++ src/java/guessNumber/UserNumberBean.java
@@ -0,0 +1,49 @@
+package guessNumber;
+
+import java.io.Serializable;
+import java.util.Random;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+
+@ManagedBean(name = "UserNumberBean")
+@SessionScoped
+public class UserNumberBean implements Serializable {
+
+    Integer randomInt;
+    Integer userNumber;
+    String response;
+
+    
+    public UserNumberBean() {
+        Random randomGR = new Random();
+        randomInt = new Integer(randomGR.nextInt(10));
+        System.out.println("Duke's number: " + randomInt);
+    }
+
+    public Integer getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(Integer userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getResponse() {
+        if ((userNumber != null) && (userNumber.compareTo(randomInt) == 0)) {
+           
+            FacesContext context = FacesContext.getCurrentInstance();
+            HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
+            session.invalidate();
+
+            return "Uhul! É isso ai!";
+        } else {
+
+            
+            return "<p>Desculpe, " + userNumber + " não é.</p>"
+                    + "<p>Tente Novamente...</p>";
+        }
+    }
+}
